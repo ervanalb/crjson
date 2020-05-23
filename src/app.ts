843262ed84e21cd5c01ad1fd275870349b9f64f5
Eric Van Albert <eric@van.al>
2020-05-22 21:58:03 -0400

    Deserializing objects seems to be working

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,34 @@
 import {State, modelToJSON} from "./lib"
 
 const s = new State("user1");
+
+s.model = {data: [
+    {
+        uid: {userId: "user1", opId: 0},
+        parent: null,
+        value: "first post",
+        counter: 0,
+    },
+    {
+        uid: {userId: "user1", opId: 1},
+        parent: null,
+        value: {},
+        counter: 1,
+    },
+    {
+        uid: {userId: "user1", opId: 2},
+        parent: {userId: "user1", opId: 1},
+        index: "a",
+        value: "b",
+        counter: 0,
+    },
+    {
+        uid: {userId: "user1", opId: 3},
+        parent: {userId: "user1", opId: 1},
+        index: "c",
+        value: "d",
+        counter: 0,
+    }
+]};
+
 console.log(modelToJSON(s.model));
--- src/lib.ts
+++ src/lib.ts
@@ -13,20 +13,22 @@ interface UniqueID {
     opId: number;
 }
 
+type Datum = Atom | (Atom & InArray) | (Atom & InObject);
+
 // A datum is an atomic value i.e. one whose merge function returns one of the two arguments rather than an amalgamation of them.
-interface Datum {
+interface Atom {
     uid: UniqueID;
     parent: UniqueID | null;
     value: any;
     counter: number; // Lamport counter
 }
 
-// This is a mixin type to "Datum" to indicate that it is stored in an array.
+// This is a mixin type to "Atom" to indicate that it is stored in an array.
 interface InArray {
     index: Array<number>; // An array index is done using a vector.
 }
 
-// This is a mixin type to "Datum" to indicate that it is stored in an object.
+// This is a mixin type to "Atom" to indicate that it is stored in an object.
 interface InObject {
     index: string;
 }
@@ -80,7 +82,16 @@ export function compare(first: Comparable, second: Comparable): number {
 }
 
 // Compare UIDs. Returns negative if first < second, positive if first > second, and zero if they are equal.
-export function compareUIDs(first: UniqueID, second: UniqueID) {
+export function compareUIDs(first: UniqueID | null, second: UniqueID | null) {
+    if (first === null && second === null) {
+        return 0;
+    }
+    if (first === null && second !== null) {
+        return -1;
+    }
+    if (first !== null && second === null) {
+        return 1;
+    }
     if (first.userId != second.userId) {
         return compare(first.userId, second.userId);
     }
@@ -88,12 +99,12 @@ export function compareUIDs(first: UniqueID, second: UniqueID) {
 }
 
 // Filters a model on a given parent
-export function filterData(data: Array<Datum>, parent: UniqueID): Array<Datum> {
-    return data.filter(item => item.parent == parent);
+export function filterChildren(data: Array<Datum>, parent: UniqueID): Array<Datum> {
+    return data.filter(item => compareUIDs(item.parent, parent) == 0);
 }
 
 // Returns the best atom candidate in a list
-export function mergeAtoms(data: Array<Datum>): Datum {
+export function mergeAtoms(data: Array<Datum>): Datum | null {
     if (data.length == 0) {
         return null;
     }
@@ -111,29 +122,72 @@ export function mergeAtoms(data: Array<Datum>): Datum {
         } else {
             throw "Could not find ordering between objects";
         }
-    }).value;
+    });
+}
+
+// Groups a flat list of Datums by their string indices (for datums in objects)
+export function groupByStringIndex(data: Array<Datum>): { [key: string]: Array<Datum>; } {
+    const result = {};
+
+    data.forEach(d => {
+        const datum = <Datum & InObject>d;
+        if (datum.index !== undefined && typeof datum.index == "string") {
+            if (result[datum.index] !== undefined) {
+                result[datum.index].push(datum);
+            } else {
+                result[datum.index] = [datum];
+            }
+        }
+    });
+
+    return result;
+}
+
+// Array.map, but for object
+export function objectMap(o: object, f: (item: any, key: any, obj: object) => any): object {
+    const result = {};
+    for (const key in o) {
+        result[key] = f(o[key], key, o);
+    }
+    return result;
 }
 
 // Converts a Model into its JSON representation
 export function modelToJSON(model: Model): JSONType {
-    const root = mergeAtoms(filterData(model.data, rootParentUID));
-    return root;
-}
+    function getArray(parent: UniqueID) {
+        const data = filterChildren(model.data, parent);
+        return [];
+    }
 
-// The UID of the parent of the root element
-const rootParentUID = {
-    userId: null,
-    opId: 0,
-};
+    function getObject(parent: UniqueID) {
+        const children = filterChildren(model.data, parent);
+        const grouped = groupByStringIndex(children);
+        return objectMap(grouped, getValue);
+    }
+
+    function getValue(data: Array<Datum>) {
+        const datum = mergeAtoms(data);
+        if (datum === null) {
+            return null;
+        }
+        const typeOfValue = betterTypeOf(datum.value);
+        if (typeOfValue == "array") {
+            return getArray(datum.uid);
+        } else if (typeOfValue == "object") {
+            return getObject(datum.uid);
+        } else {
+            return <Datum>datum.value;
+        }
+    }
+
+    return getValue(filterChildren(model.data, null)); // null parent indicates root object
+}
 
 export class State {
     uid: UniqueID;
     model: Model;
 
     constructor(userId: Comparable) {
-        if (userId === null) {
-            throw "userId cannot be null";
-        }
         this.uid = {
             userId: userId,
             opId: 0,
