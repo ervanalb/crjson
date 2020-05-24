fa5164f62a11d2d5d60d29590013ba225bc67695
Eric Van Albert <eric@van.al>
2020-05-24 17:26:35 -0400

    Set up webpack and webpage deploying

---
+++ index.js
@@ -0,0 +1,3 @@
+export * from "./src/state";
+export * from "./src/local";
+export * from "./src/datachannel";
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const distPath = path.resolve(__dirname, "dist");
+module.exports = (env, argv) => {
+  return {
+    devServer: {
+      contentBase: distPath,
+      compress: argv.mode === 'production',
+      host: "0.0.0.0",
+      port: 8000
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    entry: './index.js',
+    output: {
+      path: distPath,
+      filename: "crjson.js",
+      library: "crjson"
+    },
+    plugins: [
+      new CopyWebpackPlugin([
+        { from: './static', to: distPath }
+      ]),
+    ],
+    watch: argv.mode !== 'production',
+    devtool: 'inline-source-map',
+  };
+};
